a0fd2e7863a8d55e0649c34f599ddeda8343cefd
Eric Vieira Pereira <86498398+ericvpereira@users.noreply.github.com>
2025-10-21 20:22:45 -0300

    Apenas entrada de dados

---
+++ alturas/src/Application/Program.java
@@ -0,0 +1,29 @@
+package Application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Quantas pessoas serao digitadas? ");
+        int n = sc.nextInt();
+
+        String[] nome = new String[n];
+        int[] idade = new int[n];
+        double[] altura = new double[n];
+        for (int i = 0; i < n; i++) {
+            System.out.println("Dados da " + (i + 1) + "a pessoa:");
+            System.out.print("Nome: ");
+            nome[i] = sc.next();
+            System.out.print("Idade: ");
+            idade[i] = sc.nextInt();
+            System.out.print("Altura: ");
+            altura[i] = sc.nextDouble();
+            sc.nextLine();
+        }
+    }
+    
+}
